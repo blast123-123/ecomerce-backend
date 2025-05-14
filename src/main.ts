7d2722308a4a80d413943b69290804906f633126
blast123-123 <joshepcardenas123@gmail.com>
2025-05-14 00:47:43 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { InitialApp } from './config/initial-app'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { InitialApp } from './config/initial-app';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await InitialApp(app)
+  const app = await NestFactory.create(AppModule);
+
+  // 🔥 Habilita la carpeta "uploads" como pública
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+
+  // Configuración de tu app
+  await InitialApp(app);
 }
-bootstrap()
+bootstrap();
